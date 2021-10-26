a991f50e53302b8009455f4d01308491326ba227
tomwang1024 <annanwang@hotmail.com>
2021-10-26 17:46:51 -0400

    Update form on frontend and add routes to update/save feed data in the form of json/csv

--- app/Http/Controllers/FeedDataController.php
+++ app/Http/Controllers/FeedDataController.php
@@ -11,7 +11,7 @@ class FeedDataController extends Controller
 
     public function __construct()
     {
-        $this->feedDataService = new FeedDataService();
+        if (!$this->feedDataService) $this->feedDataService = new FeedDataService();
     }
 
     public function index(Request $request) {
@@ -20,5 +20,51 @@ class FeedDataController extends Controller
         $data["staleData"] = $feedData->getStaleData();
         $data["feedData"] = $feedData->data();
         return view("feed_data", ["data" => json_encode($data)]);
+    }
+
+    public function updateStaleData(Request $request) {
+        $newData = $request->newData;
+        $newDate = $request->newDate;
+        $this->saveAsCsv($newDate, $newData);
+        $this->saveAsJson($newDate, $newData);
+        return response()->json(["success" => true], 200);
+    }
+
+    private function saveAsJson($newDate, $data) {
+        $feedData = $this->feedDataService->getFeedData();
+        if (!$feedData) return true; // empty data return
+        $feedData->updateData($newDate, $data);
+        $fileName = time().'_feed_data.json';
+        $fp = fopen($fileName, 'w');
+        fwrite($fp, $feedData->json());
+        fclose($fp);
+        return true;
+    }
+
+    private function saveAsCsv($newDate, $data) {
+        if (!count($data)) return true; // empty data return
+        $fileName = time().'_feed_data.csv';
+
+        $file = fopen($fileName, 'w');
+        fputcsv($file, array("New Date", $newDate));
+
+        foreach ($data as $fundId => $datum) {
+            $aum = $datum['aum'];
+            $fundName = $datum['name'];
+            $row = array($fundName, $aum);
+            fputcsv($file, $row); // append first row with fundName, and its aum
+
+            $series = $datum['series'];
+            foreach ($series as $seriesId => $seriesData) {
+                $latestNav = $seriesData['latest_nav'];
+                $row = array('', $seriesId, $latestNav['value']);
+                fputcsv($file, $row); // append row with seriesId, and its NAV value
+            }
+            $row = array();
+            fputcsv($file, $row); // append empty row to separate fundIds
+        }
+        fclose($file);
+
+        return true;
     }
 }
--- resources/js/components/FeedData.vue
+++ resources/js/components/FeedData.vue
@@ -3,16 +3,16 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
 
-                <form>
+                <form id="stale-data-form">
                     <div class="form-group row">
                         <label class="sr-only col-sm-2 col-form-label" for="new-date">New Date</label>
                         <div class="input-group col-sm-4">
                             <div class="input-group-prepend">
                                 <div class="input-group-text">New Date</div>
                             </div>
-                            <input type="text" class="form-control" id="new-date" placeholder="New Date">
+                            <input required type="date" class="form-control" id="new-date" placeholder="New Date" v-model="newDate">
                         </div>
-                        <button class="btn btn-info">Submit</button>
+                        <button class="btn btn-info" type="submit" v-on:click.preventDefault="submitForm()">Submit</button>
                     </div>
 
                     <template v-for="(fundData, fundId) in staleData">
@@ -21,14 +21,14 @@
                                 <div class="form-group row">
                                     <label :for="fundId" class="col-sm-4 col-form-label">{{ fundData.name }}</label>
                                     <div class="col-sm-4">
-                                        <input type="text" class="form-control" :id="fundId" :placeholder="'AUM: ' + fundData.aum">
+                                        <input type="number" step="any" class="form-control" :id="fundId" :placeholder="'AUM: ' + fundData.aum" v-model="fundData.aum">
                                     </div>
                                     <div class="col-sm-4"></div>
                                     <template v-for="(seriesData, seriesId) in fundData.series">
                                         <div class="col-sm-4"></div>
                                         <label :for="seriesId" class="col-sm-4 col-form-label">{{ seriesId }}</label>
                                         <div class="col-sm-4">
-                                            <input type="text" class="form-control" :id="seriesId" :placeholder="'NAV: ' + seriesData.latest_nav.value">
+                                            <input type="number" step="any" class="form-control" :id="seriesId" :placeholder="'NAV: ' + seriesData.latest_nav.value" v-model="seriesData.latest_nav.value">
                                         </div>
                                     </template>
                                 </div>
@@ -44,6 +44,7 @@
 </template>
 
 <script>
+import moment from "moment/moment.js";
 export default {
     name: "FeedData",
     props: {
@@ -51,11 +52,27 @@ export default {
     },
     data() {
         return {
-            staleData: this.data.staleData ?? {}
+            staleData: this.data.staleData ?? {},
+            newDate: moment().format('YYYY-MM-DD'),
         }
     },
     mounted() {
         console.log('Component mounted.')
+    },
+    methods: {
+        submitForm() {
+            let form = $("#stale-data-form")[0];
+            if (!form || !form.checkValidity()) return;
+            if (!window.confirm("Are you sure to update?")) return;
+            let request = {
+                    newData: this.staleData,
+                    newDate: this.newDate
+                },
+                api = "/api/app/feed_data/update_stale_data";
+            axios.post(api, request).then(response => {
+                console.log(response.data);
+            })
+        }
     }
 }
 </script>
