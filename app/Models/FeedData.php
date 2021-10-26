6a5b13ba589cbeb56cd1c8c9a4c87319ac805a03
tomwang1024 <annanwang@hotmail.com>
2021-10-26 00:25:29 -0400

    create a model for feed data

--- app/Models/FeedData.php
+++ app/Models/FeedData.php
@@ -2,15 +2,57 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
-class FeedData extends Model
+class FeedData
 {
-    use HasFactory;
 
-    public function __construct(array $attributes = [])
+    private $data = null, $current_date = null;
+
+    public function __construct($data)
     {
-        parent::__construct($attributes);
+        $this->data = $data ?? (object)[];
+        $this->current_date = Carbon::createFromFormat("Y-m-d", "2020-11-26");
+    }
+
+    public function json() {
+        return json_encode($this->data);
+    }
+
+    public function data() {
+        return $this->data;
+    }
+
+    public function toArray() {
+        return (array)$this->data;
+    }
+
+    public function getStaleData() {
+        $res = [];
+        foreach ($this->data as $fundId => $fundData) {
+            $series = $fundData->series ?? null;
+            if (!$series) continue;
+            $fundName = $fundData->name->en;
+            $res[$fundId] = [
+                "name" => "$fundName($fundId)",
+                "series" => [],
+                "aum" => $fundData->aum
+            ];
+            foreach ($series as $seriesId => $seriesData) {
+                $latestNav = $seriesData->latest_nav ?? null;
+                if (!$latestNav) continue;
+                $date = Carbon::createFromFormat('Y-m-d', $latestNav->date) ?? null;
+                if (!$date) continue;
+                $res[$fundId]['series'][$seriesId] = [];
+                if ($date < $this->current_date) {
+                    $res[$fundId]['series'][$seriesId] = $seriesData;
+                }
+                else {
+                    unset($res[$fundId]['series'][$seriesId]);
+                }
+            }
+            if (!count($res[$fundId]["series"])) unset($res[$fundId]);
+        }
+        return $res;
     }
 }
