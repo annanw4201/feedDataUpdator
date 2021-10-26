6c6faeeab69d42c6004d03cf23d054232813f8ac
tomwang1024 <annanwang@hotmail.com>
2021-10-26 00:27:06 -0400

    Show the data on the frontend

--- resources/js/components/FeedData.vue
+++ resources/js/components/FeedData.vue
@@ -1,14 +1,43 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+            <div class="col-md-12">
 
-                    <div class="card-body">
-                        I'm an example component.
+                <form>
+                    <div class="form-group row">
+                        <label class="sr-only col-sm-2 col-form-label" for="new-date">New Date</label>
+                        <div class="input-group col-sm-4">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">New Date</div>
+                            </div>
+                            <input type="text" class="form-control" id="new-date" placeholder="New Date">
+                        </div>
+                        <button class="btn btn-info">Submit</button>
                     </div>
-                </div>
+
+                    <template v-for="(fundData, fundId) in staleData">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-group row">
+                                    <label :for="fundId" class="col-sm-4 col-form-label">{{ fundData.name }}</label>
+                                    <div class="col-sm-4">
+                                        <input type="text" class="form-control" :id="fundId" :placeholder="'AUM: ' + fundData.aum">
+                                    </div>
+                                    <div class="col-sm-4"></div>
+                                    <template v-for="(seriesData, seriesId) in fundData.series">
+                                        <div class="col-sm-4"></div>
+                                        <label :for="seriesId" class="col-sm-4 col-form-label">{{ seriesId }}</label>
+                                        <div class="col-sm-4">
+                                            <input type="text" class="form-control" :id="seriesId" :placeholder="'NAV: ' + seriesData.latest_nav.value">
+                                        </div>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                    </template>
+                </form>
+
             </div>
         </div>
     </div>
@@ -22,7 +51,7 @@ export default {
     },
     data() {
         return {
-
+            staleData: this.data.staleData ?? {}
         }
     },
     mounted() {
